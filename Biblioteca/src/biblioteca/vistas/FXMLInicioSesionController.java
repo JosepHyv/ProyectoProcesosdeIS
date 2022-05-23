cce55c638311f5c5ad66b2508175a2f3878999f8
JosepHyv <josephynimoto@gmail.com>
2022-05-23 01:59:33 -0500

    Inicio de sesion Creado

--- Biblioteca/src/biblioteca/vistas/FXMLInicioSesionController.java
+++ Biblioteca/src/biblioteca/vistas/FXMLInicioSesionController.java
@@ -2,11 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package biblioteca;
+package biblioteca.vistas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -14,6 +19,20 @@ import javafx.fxml.Initializable;
  * @author JosepHy
  */
 public class FXMLInicioSesionController implements Initializable {
+
+    @FXML
+    private Label label;
+    @FXML
+    private TextField tfNumeroEmpleado;
+    @FXML
+    private Button btnIniciarSesion;
+    @FXML
+    private Label lbErrorEmpleado;
+    @FXML
+    private Label lbErrorPassword;
+    @FXML
+    private PasswordField pfPassword;
+    
 
     /**
      * Initializes the controller class.
