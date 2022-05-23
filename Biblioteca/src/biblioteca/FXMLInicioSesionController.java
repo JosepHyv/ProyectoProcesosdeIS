d8a2efacbd7d903c2757aafbf8e0826af02aef9f
JosepHyv <josephynimoto@gmail.com>
2022-05-23 01:55:36 -0500

    Inicio de sesion Creado

--- Biblioteca/src/biblioteca/FXMLInicioSesionController.java
+++ Biblioteca/src/biblioteca/FXMLInicioSesionController.java
@@ -1,31 +1,41 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package biblioteca;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
+ * FXML Controller class
  *
  * @author JosepHy
  */
 public class FXMLInicioSesionController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TextField tfNumeroEmpleado;
+    @FXML
+    private Button btnIniciarSesion;
+    @FXML
+    private Label lbErrorEmpleado;
+    @FXML
+    private Label lbErrorPassword;
+    @FXML
+    private PasswordField pfPassword;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
